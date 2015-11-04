f3d4029d45a721fa69c1061bf089740faa61ce13
lisajahrulf <flurhajlisa@gmail.com>
2015-11-04 10:33:40 +0100

    first commit

---
+++ website/Scripts/main.js
@@ -0,0 +1,64 @@
+﻿/// <reference path="angular.js" />
+
+  (function () {
+      var app = angular.module('myView', []);
+      app.controller('AboutController', function ($scope) {
+
+          var learnedSkills = [
+            {
+                name: 'Html5',
+                imgSrc: '/Img/Html5.png'
+            },
+            {
+                name: 'Css3',
+                imgSrc: '/Img/css3.png'
+            },
+            {
+                name: 'Javascript',
+                imgSrc: '/Img/javascript.png'
+            },
+            {
+                name: 'jQuery',
+                imgSrc: '/Img/jQuery.png'
+            },
+            {
+                name: 'Angular',
+                imgSrc: '/Img/Angular.png'
+            },
+            {
+                name: 'Responsive',
+                imgSrc: '/Img/responsiveWeb.png'
+            }
+          ]
+
+          $scope.skills = learnedSkills;
+      });
+ 
+      }());
+
+$('body').scrollspy({ target: '#navbar-scrollspy' });
+
+//first jquery ui 
+    $('#github, #linkedin').tooltip();
+
+//Second jquery ui
+    $(function () {
+        $("#contactBtn")
+          .button()
+          .click(function (event) {
+              event.preventDefault();
+          });
+    });
+
+    $('#contactBtn').click(function () {
+        $('#contacts').slideDown();
+    });
+
+  
+
+ 
+
+
+
+
+
